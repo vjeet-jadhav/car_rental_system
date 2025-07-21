28d4c7f91d0f31b199473427ebc2784e53044e53
Rupesh <rupeshvsharma2002@gmail.com>
2025-07-21 17:45:37 +0530

    Navbar Component added

--- ProjectCar/client/app/src/App.jsx
+++ ProjectCar/client/app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
+import Container from './Screens/Container'
+import Navbar from './Components/Navbar'
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -12,7 +15,7 @@ function App() {
        
         <Route
         path="/"
-        element={<Container/>}
+        element={<Container />}
         >
           {/* here insert you page route */}
           {/* eg 
