494e6b60a62605ce117804e9b1a75af87c021422
vishwjeet jadhav <vjadhavlatur@gmail.com>
2025-07-21 09:11:31 +0530

    server added

---
+++ Project2025/Client/car_rental_system/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+
+
+function App() {
+  
+  return (
+    <>
+      <Routes>
+        {/* Login route */}
+        <Route/>
+        {/* Registration route */}
+        <Route/>
+
+        {/* main content */}
+        <Route>
+
+        </Route>
+
+      </Routes>
+    </>
+  )
+}
+
+export default App
