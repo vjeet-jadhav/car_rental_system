faa8add6018ede5dbeb489026010cabcb013120f
sanket padul <padulsanket2927@gmail.com>
2025-07-21 10:42:17 +0530

    navigation between pages

--- Project2025/Client/app/src/App.jsx
+++ Project2025/Client/app/src/App.jsx
@@ -3,6 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
+import Home from './Screens/Home/Home'
+import Container from './Screens/Container'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 function App() {
@@ -16,11 +20,19 @@ function App() {
         <Route/>
 
         {/* main content */}
-        <Route>
-
+        <Route
+        path='/'
+        element={<Container/>}
+        >
+          <Route
+          path='/home'
+          element={<Home/>}
+          />
         </Route>
-
+        
       </Routes>
+
+        
     </>
   )
 }
---
+++ Project2025/Client/app/src/Screens/Home/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        {/* hero section */}
+      <div className='mt-20 container border-amber-400 border-4 '>
+        {/* left */}
+        <div></div>
+        {/* right */}
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
