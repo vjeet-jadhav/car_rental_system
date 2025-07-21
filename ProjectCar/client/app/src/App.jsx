af3f90b97df145d8794f0b921c1766452b62b8e2
vishwjeet jadhav <vjadhavlatur@gmail.com>
2025-07-21 12:38:36 +0530

    project template

---
+++ ProjectCar/client/app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Routes,Route } from 'react-router-dom'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+       
+        <Route
+        path="/"
+        element={<Container/>}
+        >
+          {/* here insert you page route */}
+          {/* eg 
+          <Route path="/container/home" element={<Home/>}>
+           */}
+        </Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
