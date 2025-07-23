a1b3a3a59e38d8461099a4f4cc34b103222df80d
vishwjeet jadhav <vjadhavlatur@gmail.com>
2025-07-23 20:53:17 +0530

    Agent-Registration Developed

--- ProjectCar/client/app/src/Screens/Container.jsx
+++ ProjectCar/client/app/src/Screens/Container.jsx
@@ -4,7 +4,7 @@ import { Outlet , useLocation} from 'react-router-dom'
 import Navbar from '../Components/Navbar'
 import Login from '../Components/Login'
 import CarList from './Admin/CarList'
-import AdminNav from '../Components/AdminNav'
+import AdminNav from './Admin/AdminNav'
 import Profile from '../Components/Profile'
 
 
@@ -16,11 +16,6 @@ function Container() {
 
   return (
     <div style={{ position: 'relative' }}>
-      <AdminNav />
-
-      {/* for displaying profile on edit */}
-      
-
       <Outlet />
     </div>
 
