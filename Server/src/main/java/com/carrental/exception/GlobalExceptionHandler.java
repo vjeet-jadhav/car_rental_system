46caed08146fb0fd821cbfb924dd83c2aa6cdcec
Rupesh <rupeshvsharma2002@gmail.com>
2025-08-04 10:29:35 +0530

    Api Response page added

--- Server/src/main/java/com/carrental/exception/GlobalExceptionHandler.java
+++ Server/src/main/java/com/carrental/exception/GlobalExceptionHandler.java
@@ -4,9 +4,11 @@ package com.carrental.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.carrental.dto.ApiResponse;
 
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(ApiException.class)
