0f8a64db3eed8de4f64e1011f537c1db9576277b
Rupesh <rupeshvsharma2002@gmail.com>
2025-08-04 09:17:08 +0530

    Added UserDto and Required Classes

---
+++ Server/src/main/java/com/carrental/dao/UserDaoInterface.java
@@ -0,0 +1,9 @@
+package com.carrental.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.carrental.entity.User;
+
+public interface UserDaoInterface extends JpaRepository<User, Long>{
+
+}
