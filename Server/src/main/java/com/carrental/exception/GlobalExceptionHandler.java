6848685f778efbc039dcbb029169ce4d4460c5bb
Rupesh <rupeshvsharma2002@gmail.com>
2025-08-04 10:03:39 +0530

    Added Api Exception and Modified Global Exception Handler

--- Server/src/main/java/com/carrental/exception/GlobalExceptionHandler.java
+++ Server/src/main/java/com/carrental/exception/GlobalExceptionHandler.java
@@ -1,6 +1,16 @@
 // GlobalExceptionHandler.java - placeholder
 package com.carrental.exception;
 
-public class GlobalExceptionHandler {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.carrental.dto.ApiResponse;
+
+public class GlobalExceptionHandler {
+	
+	@ExceptionHandler(ApiException.class)
+	public ResponseEntity<?> handelApiException(ApiException e){
+		return ResponseEntity.status(HttpStatus.CONFLICT).body(new ApiResponse(e.getMessage()));
+	}
 }
