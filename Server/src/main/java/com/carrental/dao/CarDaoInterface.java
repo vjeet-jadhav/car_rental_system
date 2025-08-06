d99f44c088a80179757a75e16ac882e13a46e370
D4-89636-Pritish <pritishhingangave@gmail.com>
2025-08-06 15:49:06 +0530

    car dao interface is modified as per need

--- Server/src/main/java/com/carrental/dao/CarDaoInterface.java
+++ Server/src/main/java/com/carrental/dao/CarDaoInterface.java
@@ -1,9 +1,19 @@
 package com.carrental.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.carrental.dto.CarResponseDTO;
 import com.carrental.entity.Car;
 
 public interface CarDaoInterface extends JpaRepository<Car, Long> {
+
+	List<Car> findByHostId(Long userId);
+
+	boolean existsByRcNumber(String rcNumber);
 
 }
