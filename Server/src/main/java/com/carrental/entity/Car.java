0e966e36e1fc6c3141e4b13b4835d01bc001d78d
D4-89636-Pritish <pritishhingangave@gmail.com>
2025-08-06 15:54:34 +0530

    list of ratings added in car table

--- Server/src/main/java/com/carrental/entity/Car.java
+++ Server/src/main/java/com/carrental/entity/Car.java
@@ -1,13 +1,16 @@
 package com.carrental.entity;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
@@ -49,6 +52,12 @@ public class Car extends BaseEntity{
 	@Column(name="daily_rate", nullable = false)
 	private double dailyRate;
 	
+	@Column(name="sheduled_from", nullable = true)
+	private LocalDate sheduledFrom;
+	
+	@Column(name="sheduled_Till", nullable = true)
+	private LocalDate sheduledTill;
+	
 	@Enumerated(EnumType.STRING)
 	@Column(name="car_status", nullable = false)
 	private CarStatus status;
@@ -66,7 +75,7 @@ public class Car extends BaseEntity{
 	@Column(name="seat_capacity", nullable = false )
 	private int seatCapacity;
 	
-	@Column(name="approved_at", nullable = false)
+	@Column(name="approved_at")
 	private LocalDate approvedAt;
 	
 	@ManyToOne
@@ -74,10 +83,29 @@ public class Car extends BaseEntity{
 	private User host;
 	
 	@ManyToOne
-	@JoinColumn(name = "agent_id", nullable = false)
+	@JoinColumn(name = "agent_id")
 	private User agent;
 	
-	@OneToOne(mappedBy = "car")
-	@JoinColumn(name = "addrress_id", nullable = false)
+	@OneToOne(mappedBy = "car", cascade = CascadeType.ALL, orphanRemoval = true)
 	private Address address;
+	
+	@OneToMany(mappedBy = "car", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Rating> ratings;
+	
+	
+//	helpers methods that helps
+	
+//	add Address to car
+	public void addAddress(Address obj)
+	{
+		this.setAddress(obj);
+		obj.setCar(this);
+	}
+	
+// remove Address from car
+	public void removeAddress(Address obj)
+	{
+		this.setAddress(null);
+		obj.setCar(null);
+	}
 }
