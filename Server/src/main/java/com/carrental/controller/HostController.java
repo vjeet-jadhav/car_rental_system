65812b1a5334a1dcd4a7a4d0ccf038c2f5ab3f55
D4-89636-Pritish <pritishhingangave@gmail.com>
2025-08-06 15:42:56 +0530

    getMycars api is added in host controller

--- Server/src/main/java/com/carrental/controller/HostController.java
+++ Server/src/main/java/com/carrental/controller/HostController.java
@@ -2,6 +2,7 @@ package com.carrental.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,11 @@ public class HostController {
 
 	private final HostServiceImpl hostService;
 	
-	@GetMapping("/become-host/{userId}")
-	public ResponseEntity<?> getMyCars(@PathVariable Long userId)
+	@GetMapping()
+	public ResponseEntity<?> getMyCars()
 	{
+		Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getDetails();
+		System.out.println("HostController ke getMycars ke under hu padul saheb...");
 		//calling service method
 		return ResponseEntity.status(HttpStatus.OK)
 				.body(hostService.getMyCars(userId));
