1d4dc153f6e7a7bffb4b31fc24da99dff2475918
D4-89636-Pritish <pritishhingangave@gmail.com>
2025-08-08 13:23:59 +0530

    changes commited

--- ProjectCar/client/app/src/Services/user.js
+++ ProjectCar/client/app/src/Services/user.js
@@ -1,29 +1,7 @@
+// import {axios} from "axios"
 
-import axios from "axios"
-import { config } from "./config";
-
-// USER SIGNUP
-export async function userSignUp({firstName,lastName,email,password,city,state,zipCode,mob_num})
+export async function userSignUp({firstName,lastName,email,password,confirmpassword,city,state,zipCode,mob_num})
 {
-    const body = {firstName,lastName,email,password,city,state,zipCode,mob_num};
-    const url = `${config.serverUrl}/user/signup`;
-    try{
-        const result = await axios.post(url,body);
-        console.log(result);
-    }catch(e)
-    {
-        console.log(e);
-    }
-}
-
-export async function loginUser(email, password,city) {
-  try {
-    const body = { email, password , city};
-    const url = `${config.serverUrl}/user/signin`;
-    const response = await axios.post(url, body);
-    console.log(response)
-    return response.data;
-  } catch (ex) {
-    console.log(`Exception :`, ex);
-  }
+    const body = {firstName,lastName,email,password,confirmpassword,city,state,zipCode,mob_num};
+    console.log(body);
 }
